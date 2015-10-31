85d6bf767c1105e4b215d3a08fa35bedc8af479c
Zaixer <jacobsen_dennis@hotmail.com>
2015-10-31 21:21:39 +0100

    Changed to space game with physics

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance;
+    
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        var position = Camera.main.WorldToScreenPoint(transform.position);
+        var direction = Input.mousePosition - position;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        Debug.Log(transform.rotation.z);
+    }
+}
