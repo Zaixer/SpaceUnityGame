eb6a2630f05ed8a48b6d0ce2d5b9a7d331faab31
Zaixer <jacobsen_dennis@hotmail.com>
2015-11-01 12:05:04 +0100

    Moving background with player

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    private Renderer _renderer;
+    private const float SpeedFactor = 0.1f;
+
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        UpdateOffsetRelativeToPlayer();
+    }
+
+    private void UpdateOffsetRelativeToPlayer()
+    {
+        _renderer.material.mainTextureOffset = new Vector2(0f, PlayerController.Instance.Position.y * SpeedFactor);
+    }
+}
