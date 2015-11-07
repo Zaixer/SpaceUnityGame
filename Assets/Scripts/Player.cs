7efb73cb03550c0c322222a885b20dc5249fe58a
Zaixer <jacobsen_dennis@hotmail.com>
2015-11-07 20:44:04 +0100

    Max turn angles and turn resistance

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ public class Player : MonoBehaviour
     public static Player Instance;
     public Vector3 Position { get { return transform.position; } }
 
+    private const float MinAngle = 200f;
+    private const float MaxAngle = 340f;
+    private const float MaxAngleChangePerFrame = 10f;
+
     void Awake()
     {
         Instance = this;
@@ -12,14 +16,38 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        RotateTowardsMousePosition();
+        RotateTowardsPointer();
     }    
 
-    private void RotateTowardsMousePosition()
+    private void RotateTowardsPointer()
     {
-        var position = Camera.main.WorldToScreenPoint(transform.position);
-        var direction = Input.mousePosition - position;
-        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90f;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        var newAngle = GetNewAngle();
+        transform.rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+    }
+
+    private float GetNewAngle()
+    {
+        var oldAngle = transform.eulerAngles.z;
+        var angleToPointer = GetAngleToPointer();
+        if (angleToPointer > oldAngle || angleToPointer < 90f)
+        {
+            if (angleToPointer < 90f)
+            {
+                return Mathf.Min(oldAngle + MaxAngleChangePerFrame, MaxAngle);
+            }
+            return Mathf.Min(oldAngle + MaxAngleChangePerFrame, angleToPointer, MaxAngle);
+        }
+        if (angleToPointer < oldAngle || angleToPointer > 90f)
+        {
+            return Mathf.Max(oldAngle - MaxAngleChangePerFrame, angleToPointer, MinAngle);
+        }
+        return oldAngle;
+    }
+
+    private float GetAngleToPointer()
+    {
+        var directionToPointer = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angleToPointer = RotationHelper.GetRotationAngleAroundZInDegrees(directionToPointer.x, directionToPointer.y);
+        return angleToPointer;
     }
 }
---
+++ Assets/Scripts/RotationHelper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RotationHelper
+{
+    public static float GetRotationAngleAroundZInDegrees(float x, float y)
+    {
+        float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+        if (angle < 0)
+        {
+            angle += 360;
+        }
+        return angle;
+    }
+}
