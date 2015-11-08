9e4fb057bb141f6601a33a2338e2327aec35a31f
Zaixer <jacobsen_dennis@hotmail.com>
2015-11-08 11:36:45 +0100

    Added logic for score and fuel

---
+++ Assets/Scripts/Fuel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Fuel : MonoBehaviour
+{
+    public int CurrentFuel { get { return _collectedFuel - Mathf.Abs(Mathf.RoundToInt(Player.Instance.Position.y)); } }
+
+    private Text _text;
+    private int _collectedFuel = 100;
+    
+    void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        _text.text = CurrentFuel.ToString();
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static Score Instance;
+    public int CurrentScore { get { return Mathf.Abs(Mathf.RoundToInt(Player.Instance.Position.y)); } }
+
+    private Text _text;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+    
+    void Update()
+    {
+        _text.text = CurrentScore.ToString();
+    }
+}
