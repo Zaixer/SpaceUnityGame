c3ab1a77c49c28c3728fe7c7a535ea93039fc830
Zaixer <jacobsen_dennis@hotmail.com>
2015-11-08 18:08:39 +0100

    Changed to spawn asteroids in regular spacing

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,22 +2,25 @@
 
 public class Spawner : MonoBehaviour
 {
-    private const float SpawnChance = 1f / 200f;
+    private const float AsteroidHorizontalSpacing = 3f;
+    private const float AsteroidVerticalRange = 2.6f;
+    private float _horizontalLocationOfLastAsteroidSpawn;
     
     void Update()
     {
-        SpawnRandomly();
+        CheckAsteroidSpawning();
     }
 
-    private void SpawnRandomly()
+    private void CheckAsteroidSpawning()
     {
-        var shouldSpawn = Random.value <= SpawnChance;
+        var shouldSpawn = Mathf.Abs(Player.Instance.Position.y) >= _horizontalLocationOfLastAsteroidSpawn + AsteroidHorizontalSpacing;
         if (shouldSpawn)
         {
             var asteroid = Resources.Load<GameObject>("Asteroid");
-            var position = new Vector3(Random.Range(-3.6f, 3.6f), transform.position.y);
+            var position = new Vector3(Random.Range(-AsteroidVerticalRange, AsteroidVerticalRange), transform.position.y);
             var rotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
             Instantiate(asteroid, position, rotation);
+            _horizontalLocationOfLastAsteroidSpawn = Mathf.Abs(Player.Instance.Position.y);
         }
     }
 }
