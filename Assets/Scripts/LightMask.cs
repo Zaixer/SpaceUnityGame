844a6c1ee58ba76994ceb6054df5e66346cbeb3d
Zaixer <jacobsen_dennis@hotmail.com>
2016-01-01 22:18:52 +0100

    Light mask follow player

---
+++ Assets/Scripts/LightMask.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class LightMask : MonoBehaviour
+{
+    public float VerticalOffsetToPlayer = 1.5f;
+
+    void Update()
+    {
+        UpdatePositionRelativeToPlayer();
+    }
+
+    private void UpdatePositionRelativeToPlayer()
+    {
+        transform.position = new Vector3(Player.Instance.Position.x, Player.Instance.Position.y - VerticalOffsetToPlayer);
+        transform.rotation = Player.Instance.transform.rotation;
+    }
+}
